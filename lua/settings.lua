49967c327f73a35f74484877cd80e3515ed924df
risaput <arisdevian11@gmail.com>
2023-10-31 06:44:52 +0900

    "Change some settings"

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,31 +1,36 @@
+local set = vim.opt
+
 vim.cmd [[
-syntax on
-filetype on
-filetype plugin on
-filetype indent on
-
-set termguicolors
-set background=dark
-colorscheme spaceduck
-
-set noshowmode
-set laststatus=2
-set shortmess+=c
-set completeopt=menuone,noinsert
-set clipboard=unnamed
-set number
-set nowrap
-set showtabline=2
-set shiftwidth=2
-set tabstop=2
-set expandtab
-set linebreak
-set breakindent
-set autoindent
-set smartindent
-set breakindentopt=shift:2
-set backspace=2
-set pumheight=10
-set pumwidth=10
-set sidescrolloff=15
+  syntax on
+  filetype on
 ]]
+
+--  Color settings
+set.termguicolors = true
+set.background = 'dark'
+vim.cmd 'colorscheme spaceduck'
+
+-- Basic settings
+set.showmode = false
+set.laststatus = 2
+set.number = true
+set.wrap = false
+set.pumheight = 10
+set.pumwidth = 10
+set.sidescrolloff= 15
+set.shortmess = set.shortmess + 'c'
+set.completeopt = 'menuone,noinsert'
+set.clipboard= 'unnamed'
+set.backspace = 'indent,eol,start'
+
+-- Tab settings
+set.showtabline = 2
+set.shiftwidth = 2
+set.tabstop = 2
+set.expandtab= true
+set.linebreak = true
+set.breakindent = true
+set.autoindent = true
+set.smartindent = true
+set.breakindentopt = 'shift:2'
+
